4768166a0f4ce8dbc61139d1a1efd9028cb5ab42
Aleksey <pilot-1981@yandex.ru>
2022-09-12 16:10:39 +0500

    add Task_1.1

---
+++ Task_1/Program.cs
@@ -0,0 +1,24 @@
+﻿int N = 12;
+int[] array = new int[N];
+for (int i = 0; i < N; i++)
+{
+    array[i] = new Random().Next(-9, 10);
+    Console.Write($" {array[i]} ");
+}
+
+int negSum = 0;
+int posSum = 0;
+for (int i = 0; i < N; i++)
+{
+    if (array[i] < 0)
+    {
+        negSum = negSum + array[i];
+    }
+    else
+    {
+        posSum = posSum + array[i];
+    }
+}
+Console.WriteLine();
+Console.WriteLine("Сумма отрицательных чисел " + negSum);
+Console.WriteLine("Сумма положительных чисел " + posSum);

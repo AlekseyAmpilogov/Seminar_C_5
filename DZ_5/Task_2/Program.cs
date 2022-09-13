2ad26f25f8f66441e25ef358ae0d16be93679ab5
Aleksey <pilot-1981@yandex.ru>
2022-09-13 15:42:58 +0500

    add DZ_5_Task_2.1

---
+++ DZ_5/Task_2/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.Write("Задайте размерность массива: ");
+int size = Convert.ToInt32(Console.ReadLine()); // задаем размер массива
+int[] array = new int[size]; // создаем массив
+Console.Write("Получен массив: ");
+FillArray(array);
+int sumSize = 0;
+
+for (int j = 1; j < array.Length; j+=2)
+    sumSize = sumSize + array[j];
+
+Console.WriteLine();
+Console.WriteLine($"Всего {array.Length} элементов, сумма стоящих на нечетных позициях = {sumSize}");
+
+void FillArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(1, 10);
+        Console.Write($" {array[i]} "); // вывод полученного массива на экран
+    }
+}

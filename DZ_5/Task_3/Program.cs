ce05b94cd773baaa8b765963ed71cb22a34f2835
Aleksey <pilot-1981@yandex.ru>
2022-09-13 16:35:37 +0500

    add DZ_5_Task_3

---
+++ DZ_5/Task_3/Program.cs
@@ -0,0 +1,31 @@
+﻿double[] array = new double[8]; // создаем массив
+Console.Write("Получен массив: ");
+FillArray(array);
+
+double max = array[0];
+double min = array[0];
+
+for (int i = 1; i < array.Length; i++)
+{
+    if (max < array[i])
+    {
+        max = array[i];
+    }
+        if (min > array[i])
+    {
+        min = array[i];
+    }
+}
+double difference = max - min;
+
+Console.WriteLine();
+Console.WriteLine($"Разница между максимальным ({max}) и минимальным ({min}) элементами = {difference}");
+
+void FillArray(double[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(1, 10);
+        Console.Write($" {array[i]} "); // вывод полученного массива на экран
+    }
+}
